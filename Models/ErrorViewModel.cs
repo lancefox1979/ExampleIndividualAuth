41845b1137dec728e65a34b19aa9e1d261c125fd
Shannon Fisher <lancefox1979@gmail.com>
2020-02-14 20:52:17 -0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExampleIndividualAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
